5a2102a5c19ea2ba206f8c2e490ef001f6a19730
larasmey <69838304+laras61@users.noreply.github.com>
2021-09-06 20:28:28 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Laraspage</title>
+</head>
+<body>
+<style>
+* {
+  padding:0px;
+  margin:0px;
+}
+.container {
+  width:80%;
+  margin:auto;
+ }
+
+.head {
+   height:40px;
+   background:#F3F1F5;
+  text-align: right;
+    padding-right: 10px;
+    padding-top: 10px;
+}
+
+.footer {
+   height:40px;
+   background:#F3F1F5;
+  text-align: right;
+    padding-right: 10px;
+    padding-top: 10px;
+}
+
+.head a {
+  color: olive;
+    text-decoration-line: none;
+}
+
+.head a:hover {
+  color:slategray;
+    text-decoration-line: underline;
+}
+
+.head h1 {
+  
+}
+
+.ban {
+   height:250px;
+   background:#F0D9FF;
+}
+
+
+.footer {
+ 
+   height:80px;
+   background:#7F7C82;
+   text-align:right;
+   line-height:80px;
+   /* display:flex;
+   align-items:center;
+   justify-content:end !important; */
+}
+
+ .footer a {
+   color: lavender;
+   text-decoration:none;
+ }
+
+.footer a:hover {
+   color: black;
+   text-decoration:underline;
+ }
+
+.content {
+  height:600px;
+}
+
+.sidebar {
+  height: 100%;
+   width:25%;
+  list-style: none;
+  float:left;
+  background-color: lavender;
+}
+.sidebar li a {
+  display: block;
+  padding: 25px;
+  text-decoration: none;
+  color: lightslategray;
+  font-weight: bold;
+  border-bottom: 1px solid silver;
+  transition: background-color 0.5s;
+}
+.sidebar li a:hover {
+  background-color: grey;
+  color:lavender;
+}
+
+</style>
+
+
+  <div class="container">
+        <div class="head">
+          <a href="">Home</a>&nbsp;|
+          <a href="">Profile</a>&nbsp;|
+          <a href="">Contact</a>
+        </div>  
+    
+    
+     <div class="ban">
+         <center><img style="width:50%;height:40%;padding:10px;" src="https://frac.org/wp-content/uploads/library-shelves-banner-photo.jpg"></center>
+        </div>  
+    
+         <div class="content">
+           
+        
+  <ul class="sidebar">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">User</a></li>
+    <li><a href="#">Gallery</a></li>
+    <li><a href="#">Setting</a></li>
+    <li><a href="#">Help</a></li>
+  </ul>
+      
+      <div style="width:75%;height:100%;background:#BFA2DB;float:right;">
+       <center><h1 style="color:beige;padding:10px;">Membaca Membuka wawasan</h1></center><br>
+       <p style="color:beige;padding:10px;"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa aut sit, enim nisi unde consequatur, aspernatur
+        officia at placeat minima quis itaque optio natus aliquam facilis harum maxime molestias voluptatum fuga. Ipsum
+        obcaecati voluptate deserunt architecto, necessitatibus sequi, error aut excepturi ullam expedita voluptas iste?
+        Magnam voluptas nihil facilis nemo est placeat quas numquam optio ratione, iusto voluptates magni sunt,
+        doloribus cupiditate accusamus, temporibus modi facere debitis. Nam, vel error! Debitis nulla porro eius tempore
+        id sint quis similique impedit hic, tempora laboriosam velit quo. Ratione architecto sed sunt eius reprehenderit
+        necessitatibus doloribus, quos quia incidunt aspernatur. Repudiandae aliquid eveniet mollitia repellendus
+        deserunt quo quisquam. Ipsam culpa magni nulla mollitia, iste aliquam voluptate eaque in doloribus autem dolorum
+        quaerat alias quisquam! Vitae doloremque corrupti eius, laboriosam inventore dignissimos est laborum qui fugit
+        totam quisquam non perspiciatis beatae veritatis voluptatibus ducimus debitis. </p>
+        <p style="color:beige;padding:10px;">Adipisci iste consectetur enim aspernatur magni ad similique possimus veniam vero aperiam illo velit, dolor suscipit aut vitae sapiente
+        praesentium, quia dolores officia dolorem? Modi quidem totam, consequatur voluptatibus dolore tempora similique
+        ullam, illum et nesciunt magni maxime quod inventore ab officiis mollitia perferendis voluptate dolor voluptatem
+        aut dignissimos? Incidunt sed nulla non dicta eaque, voluptates qui assumenda optio error blanditiis
+        perspiciatis temporibus, esse a quibusdam maiores sint inventore molestiae nobis nemo! Officia eum quibusdam,
+        perspiciatis in iusto, aliquid accusamus qui maxime iste, minima maiores! Commodi quae porro voluptas!
+        Aspernatur deserunt est molestiae eveniet repellendus, nisi voluptatibus maiores! Voluptatibus tenetur
+        consectetur voluptatum molestias error provident eum quas ex consequatur minus iste nam deleniti consequuntur,
+        soluta commodi sint incidunt doloremque architecto nulla repellendus necessitatibus temporibus sunt deserunt.
+        Molestiae dolor totam aliquid perspiciatis adipisci nostrum eveniet dolore, tempora omnis exercitationem
+        eligendi laboriosam nesciunt a architecto error distinctio cum, ipsam ducimus voluptatibus sed neque. Veniam
+        iure dolor corporis aperiam quod commodi nam.</p>
+      </div>
+         </div>
+          
+        <div class="footer">
+          <ul>
+                <a href="">Home</a>&nbsp;|
+                <a href="">Profile</a>&nbsp;|
+                <a href="">Contact</a>
+            </ul>
+        </div>  
+    
+  </div>
+  
+</body>
+</html>
